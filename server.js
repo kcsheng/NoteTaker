656aa85ef27ec3f3de65558dccc3ade77218c535
kcsheng <kacysheng@mail.com>
2021-08-07 23:13:11 +1000

    Add get, post and delete routes

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const path = require("path");
+const uuid = require("./helpers/uuid");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => res.send("Homepage"));
+app.get("/api/notes", (req, res) => {
+  res.status(200).json(notes);
+  console.info(`${req.method} request received to get all notes`);
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a task`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      noteID: uuid(),
+    };
+
+    readFile("./db/db.json")
+      .then((data) => {
+        const dbNotes = JSON.parse(data);
+        dbNotes.push(newNote);
+        return dbNotes;
+      })
+      .then((dbNotes) => {
+        writeFile("./db/db.json", JSON.stringify(dbNotes, 0, 4));
+      })
+      .catch((err) => console.error(err));
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+    res.status(200).json(response);
+  } else {
+    res.status(404).send("Please supply both title and text!");
+  }
+});
+
+// app.get("/api/notes/:taskID");
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  // bring out the notes from db.json
+  if (id) {
+    readFile("./db/db.json")
+      .then((data) => {
+        const dbNotes = JSON.parse(data);
+        const newDbNotes = dbNotes.filter((note) => {
+          return note.noteID !== id;
+        });
+        return newDbNotes;
+      })
+      .then((newDbNotes) => {
+        writeFile("./db/db.json", JSON.stringify(newDbNotes, 0, 4));
+      })
+      .catch((err) => console.error(err));
+    const response = {
+      status: "success",
+    };
+    res.status(200).json(response);
+  } else {
+    res.status(404).send(`The ID of ${id} is not found!`);
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
